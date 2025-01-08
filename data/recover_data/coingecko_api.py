0774902fb8477f2535fd61b47e3b2e4742f033ef
arthurdsfr <arthur.desfrancais@gmail.com>
2025-01-08 20:32:34 +0100

    eth_price + market cap + volume trading 24h ok

--- data/recover_data/coingecko_api.py
+++ data/recover_data/coingecko_api.py
@@ -6,7 +6,8 @@ def fetch_eth_prices(num_days):
     params = {
         "vs_currency": "usd",
         "days": num_days,
-        "interval": "daily"
+        "interval": "daily",
+        "price_change_percentage": "7d,30d"
     }
 
     response = requests.get(url, params=params)
@@ -15,14 +16,19 @@ def fetch_eth_prices(num_days):
         data = response.json()
         prices_value = [entry[1] for entry in data['prices']]  # On récupère uniquement les prix en USD
         market_cap_value = [entry[1] for entry in data['market_caps']]
-        return prices_value, market_cap_value
+        volume_trading_24h_value = [entry[1] for entry in data['total_volumes']]
+        return prices_value, market_cap_value, volume_trading_24h_value
     else:
         print(f"Erreur lors de la récupération des données: {response.status_code}")
         return []
 
 # Récupérer les prix de l'Ethereum sur les 365 derniers jours
-eth_prices, market_cap = fetch_eth_prices(365)
+eth_prices, market_cap, volume_value = fetch_eth_prices(365)
 
+
+print(len(eth_prices))
+print(len(market_cap))
+print(len(volume_value))
 
 # Tracer le graphique
 plt.figure(figsize=(10, 5))
@@ -32,7 +38,7 @@ plt.xlabel("Jour")
 plt.ylabel("Prix en USD")
 plt.legend()
 plt.grid(True)
-plt.savefig('graph_eth_price')
+#plt.savefig('graph_eth_price')
 
 
 
