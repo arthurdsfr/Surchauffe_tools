e3c45f1fcd36f3ab80ef185095f84921d8a086ea
arthurdsfr <arthur.desfrancais@gmail.com>
2025-01-13 15:41:21 +0100

    labeled_marketcap OK

--- data/labeled_data/labeled_data_ma.py
+++ data/labeled_data/labeled_data_ma.py
@@ -2,11 +2,6 @@ import data.calculated_data.mobile_average as ma
 import numpy as np
 import matplotlib.pyplot as plt
 
-def compter_occurrences(liste):
-    count_1 = liste.count(1)
-    count_0 = liste.count(0)
-    count_neg1 = liste.count(-1)
-    return count_1, count_0, count_neg1
 
 def labeled_price_coin_sma50(coin_prices_365j):
     criteria_1 = 0.90
@@ -63,26 +58,10 @@ def labeled_price_coin_ema200(coin_prices_365j):
             labels.append(-1)
     return labels
 
-np.random.seed(42)  # Pour la reproductibilité
-jours = 365
-prix_initial = 100
-# Générer des variations journalières avec une moyenne de 0 et une écart-type de 2
-variations = np.random.normal(0, 2, jours)
-# Calculer les prix en cumulant les variations
-prix = prix_initial + np.cumsum(variations)
 
 
-labeled = labeled_price_coin_ema200(prix)
-count_1, count_0, count_neg1 = compter_occurrences(labeled)
-
-print(len(labeled))
-print(f"Nombre de 1 : {count_1}")
-print(f"Nombre de 0 : {count_0}")
-print(f"Nombre de -1 : {count_neg1}")
-
-
-plt.figure(figsize=(14, 7))
-plt.plot(prix, label='Prix simulés', color='blue', alpha=0.6)
-plt.plot(ma.ema_200(prix), label='SMA 50 jours', color='orange', linestyle='--')
-plt.grid(True)
-plt.savefig('test_ema_sma')
+#plt.figure(figsize=(14, 7))
+#plt.plot(prix, label='Prix simulés', color='blue', alpha=0.6)
+#plt.plot(ma.ema_200(prix), label='SMA 50 jours', color='orange', linestyle='--')
+#plt.grid(True)
+#plt.savefig('test_ema_sma')
---
+++ data/labeled_data/labeled_data_marketcap.py
@@ -0,0 +1,43 @@
+import data.calculated_data.marketcap_average as avg
+import numpy as np
+
+def labeled_marketcap(coin_marketcap_365j):
+    criteria_1 = 0.7 #critere + ou - 30%
+    criteria_2 = 1.3
+    labels = []
+    average_marketcap = avg.average_marketcap(coin_marketcap_365j)
+    for marketcap in coin_marketcap_365j:
+        if marketcap >= average_marketcap*criteria_2:
+            labels.append(1)
+        elif average_marketcap*criteria_1 < marketcap < average_marketcap*criteria_2:
+            labels.append(0)
+        else :
+            labels.append(-1)
+    return labels
+
+
+def compter_occurrences(liste):
+    count_1 = liste.count(1)
+    count_0 = liste.count(0)
+    count_neg1 = liste.count(-1)
+    return count_1, count_0, count_neg1
+
+np.random.seed(42)  # Pour la reproductibilité
+jours = 365
+prix_initial = 100
+# Générer des variations journalières avec une moyenne de 0 et une écart-type de 2
+variations = np.random.normal(0, 2, jours)
+# Calculer les prix en cumulant les variations
+prix = prix_initial + np.cumsum(variations)
+
+
+labeled = labeled_marketcap(prix)
+count_1, count_0, count_neg1 = compter_occurrences(labeled)
+
+print(len(labeled))
+print(f"Nombre de 1 : {count_1}")
+print(f"Nombre de 0 : {count_0}")
+print(f"Nombre de -1 : {count_neg1}")
+
+
+
