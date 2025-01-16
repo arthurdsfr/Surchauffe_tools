24c4f15358c48ca720af14226c6c94131ad85652
arthurdsfr <arthur.desfrancais@gmail.com>
2025-01-16 21:53:23 +0100

    volume ok

--- data/labeled_data/labeled_data_volume.py
+++ data/labeled_data/labeled_data_volume.py
@@ -1,15 +1,15 @@
 import data.calculated_data.volume_trading_avg as ta
-import numpy as np
+
 
 def labeled_volume(coin_volume_365j):
     criteria_1 = 0.7
-    criteria_2 = 1.3
+    criteria_2 = 1.20
     labels = []
     volume_average = ta.volume_trading_average(coin_volume_365j)
     for volume_1d in coin_volume_365j:
         if volume_1d >= volume_average*criteria_2:
             labels.append(1)
-        elif volume_average*criteria_1 < volume_1d < volume_1d*criteria_2:
+        elif volume_average*criteria_1 < volume_1d < volume_average*criteria_2:
             labels.append(0)
         else :
             labels.append(-1)
