e0fc997cb6f217289789bded68a81cccc980c5b6
arthurdsfr <arthur.desfrancais@gmail.com>
2025-01-16 20:18:42 +0100

    critere ok

--- data/labeled_data/labeled_data_ma.py
+++ data/labeled_data/labeled_data_ma.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 
 def labeled_price_coin_sma50(coin_prices_365j):
-    criteria_1 = 0.90
-    criteria_2 = 1.10
+    criteria_1 = 0.92
+    criteria_2 = 1.08
     labels = []
     sma50 = ma.sma_50(coin_prices_365j)
     for i in range(0, len(coin_prices_365j)):
@@ -18,8 +18,8 @@ def labeled_price_coin_sma50(coin_prices_365j):
     return labels
 
 def labeled_price_coin_sma200(coin_prices_365j):
-    criteria_1 = 0.92
-    criteria_2 = 1.08
+    criteria_1 = 0.89
+    criteria_2 = 1.11
     labels = []
     sma200 = ma.sma_200(coin_prices_365j)
     for i in range(0, len(coin_prices_365j)):
@@ -45,8 +45,8 @@ def labeled_price_coin_ema50(coin_prices_365j):
             labels.append(-1)
     return labels
 def labeled_price_coin_ema200(coin_prices_365j):
-    criteria_1 = 0.92
-    criteria_2 = 1.08
+    criteria_1 = 0.93
+    criteria_2 = 1.11
     labels = []
     ema200 = ma.ema_200(coin_prices_365j)
     for i in range(0, len(coin_prices_365j)):
@@ -58,10 +58,3 @@ def labeled_price_coin_ema200(coin_prices_365j):
             labels.append(-1)
     return labels
 
-
-
-#plt.figure(figsize=(14, 7))
-#plt.plot(prix, label='Prix simulés', color='blue', alpha=0.6)
-#plt.plot(ma.ema_200(prix), label='SMA 50 jours', color='orange', linestyle='--')
-#plt.grid(True)
-#plt.savefig('test_ema_sma')
--- data/labeled_data/labeled_data_marketcap.py
+++ data/labeled_data/labeled_data_marketcap.py
@@ -1,28 +1,31 @@
 import data.calculated_data.marketcap_avg as avg
+import matplotlib.pyplot as plt
+import data.recover_data.eth_from_excel as excel
 import numpy as np
 
 def labeled_marketcap(coin_marketcap_365j):
-    criteria_1 = 0.7 #critere + ou - 30%
-    criteria_2 = 1.3
+    criteria_1 = 0.89 #critere + ou - 11%
+    criteria_2 = 1.11
     labels = []
     marketcap_average = avg.average_marketcap(coin_marketcap_365j)
     for marketcap_1d in coin_marketcap_365j:
-        if marketcap_1d >= marketcap_average*criteria_2:
+        if marketcap_1d >= marketcap_average * criteria_2:
             labels.append(1)
-        elif marketcap_1d*criteria_1 < marketcap_1d < marketcap_average*criteria_2:
+        elif marketcap_average * criteria_1 <= marketcap_1d < marketcap_average * criteria_2:
             labels.append(0)
-        else :
+        else:
             labels.append(-1)
     return labels
 
 
+'''
 def compter_occurrences(liste):
     count_1 = liste.count(1)
     count_0 = liste.count(0)
     count_neg1 = liste.count(-1)
     return count_1, count_0, count_neg1
 
-'''
+
 np.random.seed(42)  # Pour la reproductibilité
 jours = 365
 prix_initial = 100
