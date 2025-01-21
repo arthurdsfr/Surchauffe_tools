5edee46a10575260cfb7d27a467f041f504beda8
arthurdsfr <arthur.desfrancais@gmail.com>
2025-01-21 17:45:10 +0100

    variation price error ok

--- data/calculated_data/variation_price.py
+++ data/calculated_data/variation_price.py
@@ -8,20 +8,32 @@ def price_variation_1d(coin_prices_365j):
         price_variation_1d.append(ecart_percentage)
     return price_variation_1d
 
+
 def price_variation_7d(coin_prices_365j):
+    if len(coin_prices_365j) < 7:
+        raise ValueError("La liste coin_prices_365j doit contenir au moins 7 éléments.")
+
     price_variation_7d = [0]
+
+    # Calcul des variations pour les 7 premiers jours
     for day in range(1, 7):
         ecart = coin_prices_365j[day] - coin_prices_365j[0]
-        ecart_percentage = (ecart/coin_prices_365j[0])*100
+        ecart_percentage = (ecart / coin_prices_365j[0]) * 100
         price_variation_7d.append(ecart_percentage)
+
+    # Calcul des variations au-delà des 7 jours
     for day in range(7, len(coin_prices_365j)):
-        ecart = coin_prices_365j[day] - coin_prices_365j[day-7]
-        ecart_percentage = (ecart/coin_prices_365j[day-7])*100
+        ecart = coin_prices_365j[day] - coin_prices_365j[day - 7]
+        ecart_percentage = (ecart / coin_prices_365j[day - 7]) * 100
         price_variation_7d.append(ecart_percentage)
 
     return price_variation_7d
 
+
 def price_variation_30d(coin_prices_365j):
+    if len(coin_prices_365j) < 30:
+        raise ValueError("La liste coin_prices_365j doit contenir au moins 30 éléments.")
+
     price_variation_30d = [0]
     for day in range(1, 30):
         ecart = coin_prices_365j[day] - coin_prices_365j[0]
