be4d5ac331565c13029a869139b1bd61933532a1
arthurdsfr <arthur.desfrancais@gmail.com>
2025-01-27 17:39:51 +0100

    ok predict model

---
+++ model/predict_model.py
@@ -0,0 +1,37 @@
+import data.calculated_data.rsi as rs
+import data.calculated_data.moving_average as ma
+import data.calculated_data.variation_price as vp
+import pandas as pd
+from joblib import load
+
+# Fonction pour faire des prédictions
+def predict_model(coin_prices_365j, marketcap_365j, volume_24h_365j):
+    # Calculer les mêmes caractéristiques que pendant l'entraînement
+    rsi = rs.rsi(coin_prices_365j)
+    sma50 = ma.sma_50(coin_prices_365j)
+    sma200 = ma.sma_200(coin_prices_365j)
+    ema50 = ma.ema_50(coin_prices_365j)
+    ema200 = ma.ema_200(coin_prices_365j)
+    vp_1d = vp.price_variation_1d(coin_prices_365j)
+    vp_7d = vp.price_variation_7d(coin_prices_365j)
+    vp_30d = vp.price_variation_30d(coin_prices_365j)
+
+    # Créer le DataFrame avec les caractéristiques calculées
+    df = pd.DataFrame({
+        'Price': coin_prices_365j,
+        'Market Cap': marketcap_365j,
+        'Volume': coin_prices_365j,
+        'RSI': rsi,
+        'SMA50': sma50,
+        'SMA200': sma200,
+        'EMA50': ema50,
+        'EMA200': ema200,
+        'VP_1d': vp_1d,
+        'VP_7d': vp_7d,
+        'VP_30d': vp_30d
+    })
+
+    model = load('eth_decision_tree_model.joblib')
+    prediction = model.predict(df)
+    return prediction
+
