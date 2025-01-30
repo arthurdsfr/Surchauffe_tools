15401ef314198d8a577dc5c9d76ba34d42cd7bbf
arthurdsfr <arthur.desfrancais@gmail.com>
2025-01-30 19:20:58 +0100

    dbeaver

--- data/recover_data/coin_from_dbeaver.py
+++ data/recover_data/coin_from_dbeaver.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 import pymysql
 
-def fetch_eth_data_from_excel():
+def fetch_data_from_excel():
 
     engine = create_engine('mysql+pymysql://root:azerty@localhost/crypto_data')
 
@@ -43,24 +43,28 @@ def fetch_60d_from_excel():
         # Charger les données
         query = '''
         SELECT 
+            `Date`,
             `Prix (USD)`,
             `MarketCap (USD)`,
-            `Volume (USD)`,
-            ORDER BY `Date` DESC LIMIT 60
+            `Volume (USD)`
         FROM solana_data
+        ORDER BY `Date` DESC
+        LIMIT 60
         '''
         # Lire les données dans un DataFrame
         df = pd.read_sql(query, engine)
 
         # Inverser l'ordre des lignes pour avoir les données du plus ancien au plus récent
         df = df.iloc[::-1]
-
+        coin_prices_60d = df['Prix (USD)'].tolist()
+        marketcap_60d = df['MarketCap (USD)'].tolist()
+        volume_24h_60d = df['Volume (USD)'].tolist()
         # Retourner le DataFrame
-        return df
+        return coin_prices_60d, marketcap_60d, volume_24h_60d
 
     except Exception as e:
         print("Une erreur s'est produite :", e)
-        return pd.DataFrame()
+        return [], [], []
 
     finally:
         engine.dispose()
