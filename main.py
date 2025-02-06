57570ecc72a02e9b5a1080b32977e7d7ce744099
arthurdsfr <arthur.desfrancais@gmail.com>
2025-02-06 20:25:18 +0100

    indicators and main OK

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from model.predict_model import predict_model
 
 
 #fetch les variables
-df, coin_prices, marketcap, volume = fetch_60d_from_excel()
+df, coin_prices, number_trades, volume_24h = fetch_60d_from_excel()
 
 #liste predictions
-prediction = predict_model(coin_prices, marketcap, volume)
+prediction = predict_model(coin_prices, number_trades, volume_24h)
 
 #plot rsi, sma, ema, vp
 plot_indicators(df, coin_prices)
--- vizualisation/plot_indicators.py
+++ vizualisation/plot_indicators.py
@@ -4,29 +4,29 @@ import data.calculated_data.rsi as rs
 import data.calculated_data.moving_average as ma
 import data.calculated_data.variation_price as vp
 
-def plot_indicators(df, coin_prices_365j):
+def plot_indicators(df, coin_prices):
     dates = np.arange(len(df))  # Création d'un axe temporel fictif
 
     fig, axs = plt.subplots(3, 1, figsize=(12, 12), sharex=True)
 
     # Prix et Moyennes Mobiles
-    axs[0].plot(dates, df['Prix (USD)'], label="Price", color='black')
-    axs[0].plot(dates, ma.sma_50(coin_prices_365j), label="SMA50", linestyle="dashed", color='blue')
-    axs[0].plot(dates, ma.sma_200(coin_prices_365j), label="SMA200", linestyle="dashed", color='red')
+    axs[0].plot(dates, df['Close'], label="Price", color='black')
+    axs[0].plot(dates, ma.sma_50(coin_prices), label="SMA50", linestyle="dashed", color='blue')
+    axs[0].plot(dates, ma.sma_200(coin_prices), label="SMA200", linestyle="dashed", color='red')
     axs[0].set_title("Prix et Moyennes Mobiles")
     axs[0].legend()
 
     # RSI
-    axs[1].plot(dates, rs.rsi(coin_prices_365j), label="RSI", color='purple')
+    axs[1].plot(dates, rs.rsi(coin_prices), label="RSI", color='purple')
     axs[1].axhline(y=70, color='r', linestyle='dashed', label="Surachat (70)")
     axs[1].axhline(y=30, color='g', linestyle='dashed', label="Survente (30)")
     axs[1].set_title("RSI")
     axs[1].legend()
 
     # Variation du prix
-    axs[2].plot(dates, vp.price_variation_1d(coin_prices_365j), label="VP_1d", color='orange')
-    axs[2].plot(dates, vp.price_variation_7d(coin_prices_365j), label="VP_7d", color='blue')
-    axs[2].plot(dates, vp.price_variation_30d(coin_prices_365j), label="VP_30d", color='red')
+    axs[2].plot(dates, vp.price_variation_1d(coin_prices), label="VP_1d", color='orange')
+    axs[2].plot(dates, vp.price_variation_7d(coin_prices), label="VP_7d", color='blue')
+    axs[2].plot(dates, vp.price_variation_30d(coin_prices), label="VP_30d", color='red')
     axs[2].set_title("Variation du Prix")
     axs[2].legend()
 
