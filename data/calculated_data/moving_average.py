8659b8769b9c9b1f5001f10f5cdaf9d49b47fe2d
arthurdsfr <arthur.desfrancais@gmail.com>
2025-02-06 19:57:14 +0100

    ok

--- data/calculated_data/moving_average.py
+++ data/calculated_data/moving_average.py
@@ -1,24 +1,22 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-def ema_50(coin_prices_365j):
-    serie_price = pd.Series(coin_prices_365j)
+def ema_50(coin_prices):
+    serie_price = pd.Series(coin_prices)
     list_ema_50 = serie_price.ewm(span=50, adjust=False).mean()
     return list_ema_50
 
-def sma_50(coin_prices_365j):
-    serie_price = pd.Series(coin_prices_365j)
+def sma_50(coin_prices):
+    serie_price = pd.Series(coin_prices)
     list_ma_50 = serie_price.rolling(window=50, min_periods=1).mean()
     return list_ma_50
 
-def ema_200(coin_prices_365j):
-    serie_price = pd.Series(coin_prices_365j)
+def ema_200(coin_prices):
+    serie_price = pd.Series(coin_prices)
     list_ema_200 = serie_price.ewm(span=200, adjust=False).mean()
     return list_ema_200
 
-def sma_200(coin_prices_365j):
-    serie_price = pd.Series(coin_prices_365j)
+def sma_200(coin_prices):
+    serie_price = pd.Series(coin_prices)
     list_ma_200 = serie_price.rolling(window=200, min_periods=1).mean()
     return list_ma_200
 
