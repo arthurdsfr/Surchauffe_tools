1d3874c72c0004b6534185a7337a125178b44992
arthurdsfr <arthur.desfrancais@gmail.com>
2025-02-06 14:40:02 +0100

    from Binance OK

--- data/recover_data/coin_from_excel.py
+++ data/recover_data/coin_from_excel.py
@@ -4,21 +4,21 @@ import pandas as pd
 def fetch_data_from_excel():
     try:
         # Définir le chemin du fichier Excel
-        file_path = "C:\\Users\\arthu\\IdeaProjects\\Surchauffe_Tools\\data\\recover_data\\solana_data.xlsx"  # À modifier avec le bon chemin
+        file_path = "C:\\Users\\arthu\\PycharmProjects\\Surchauffe_Tools\\data\\recover_data\\BTCUSDT.xlsx  "  # À modifier avec le bon chemin
 
         # Lire les données depuis l'Excel
         df = pd.read_excel(file_path, sheet_name=0)
 
         # Vérifier si les colonnes existent
-        required_columns = ['Prix (USD)', 'MarketCap (USD)', 'Volume (USD)']
+        required_columns = ['Close', 'Volume', 'Number of Trades']
         for col in required_columns:
             if col not in df.columns:
                 raise ValueError(f"La colonne '{col}' est absente du fichier Excel.")
 
         # Extraire chaque colonne en liste
-        eth_prices_365j = df['Prix (USD)'].tolist()
-        marketcap_365j = df['MarketCap (USD)'].tolist()
-        volume_24h_365j = df['Volume (USD)'].tolist()
+        eth_prices_365j = df['Close'].tolist()
+        marketcap_365j = df['Volume'].tolist()
+        volume_24h_365j = df['Number of Trades'].tolist()
 
         # Retourner les listes
         return df, eth_prices_365j, marketcap_365j, volume_24h_365j
@@ -26,7 +26,8 @@ def fetch_data_from_excel():
     except Exception as e:
         print("Une erreur s'est produite :", e)
         return [], [], []
-
+df, eth_prices_365j, marketcap_365j, volume_24h_365j = fetch_data_from_excel()
+print(df)
 
 def fetch_60d_from_excel():
     try:
---
+++ data/recover_data/coin_to_excel_binance.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+
+def get_historical_data():
+    url = "https://api.binance.com/api/v3/klines"
+    params = {
+        'symbol': "ETHUSDT",
+        'interval': "1d",
+        'limit': 1000  # Max 1000 bougies par requête
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        df = pd.DataFrame(data, columns=[
+            "Open Time", "Open", "High", "Low", "Close", "Volume",
+            "Close Time", "Quote Asset Volume", "Number of Trades",
+            "Taker Buy Base Volume", "Taker Buy Quote Volume", "Ignore"
+        ])
+        df_data = df[["Close", "Volume", "Number of Trades"]]
+        return df_data
+    else :
+        print("Erreur API Binance")
+        return None
+
+# df_data = get_historical_data()
+# df_data.to_excel("ETHUSDT.xlsx")
+
+
